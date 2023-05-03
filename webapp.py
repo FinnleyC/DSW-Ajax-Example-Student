11140574556c7295071ae5a67b1003bc28211ad4
FinnleyC <41851897+FinnleyC@users.noreply.github.com>
2023-05-03 13:59:44 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,8 +7,9 @@ app.debug = True #Change this to False for production
 
 @app.route('/t', methods=['POST'])
 def t():
-    print(request.form.to_dict())
-    return Markup('<h2>New div1</h2> <p>This is the new content, from the server</p>')
+    h = request.form['head']
+    b = request.form['body']
+    return Markup('<h2>'+h+'</h2> <p>'+b+'</p>')
 
 @app.route('/')
 def home():
